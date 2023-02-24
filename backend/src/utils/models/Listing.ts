de8b670a6751d6fa3d2c0da2b83e23a44c931503
Nicole Diaz <ndiaz18@cnm.edu>
2023-02-24 13:42:48 -0700

    finished the insert listing function

--- backend/src/utils/models/Listing.ts
+++ backend/src/utils/models/Listing.ts
@@ -13,6 +13,7 @@ export interface Listing {
 }
 
 export async function insertListing (listing: Listing): Promise<string> {
-    const {listingProfileId, listingCategoryId, listingCondition, listingClaimed, listingDescription, listingImageUrl, listingName} = listing
-    await sql `INSERT INTO listing (listing_id, listing_category_id, listing_profile_id, listing_condition, listing_description, listing_image_url, listing_name) `
+    const {listingCategoryId, listingProfileId, listingCondition, listingClaimed, listingDescription, listingImageUrl, listingName} = listing
+    await sql `INSERT INTO listing (listing_id, listing_category_id, listing_profile_id, listing_condition, listing_claimed, listing_date, listing_description, listing_image_url, listing_name) values (gen_random_uuid(), ${listingCategoryId}, ${listingProfileId}, ${listingCondition}, ${listingClaimed}, now(), ${listingDescription}, ${listingImageUrl}, ${listingName})`
+    return 'Listing created successfully'
 }
