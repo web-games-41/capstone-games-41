aec8531273fb6a59459b1b80ed6565de050f6809
Nicole Diaz <ndiaz18@cnm.edu>
2023-03-03 10:45:42 -0700

    added the initial homepage

--- frontend/src/ui/App.jsx
+++ frontend/src/ui/App.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import { Home } from './Home.jsx'
 import { FourOhFour } from './FourOhFour'
+import {Navigation} from "./Navigation";
 
 
 export function App() {
     return (
         <>
             <BrowserRouter>
+                <Navigation></Navigation>
                 <Routes>
                     <Route  path='/' element={<Home />} />
                     <Route path={"*"} element={<FourOhFour />} />
