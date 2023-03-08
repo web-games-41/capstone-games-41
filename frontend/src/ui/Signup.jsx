1aa2f11ae310d4173af33e9be4ad1e5c55316f43
Jared Diaz <jdiaz97@cnm.edu>
2023-03-08 16:15:36 -0700

    Finished sign-up Static UI

--- frontend/src/ui/Signup.jsx
+++ frontend/src/ui/Signup.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-import {Button, Container} from "react-bootstrap";
+import {Button, Col, Container, Image, Row} from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 
 
 export function Signup() {
     return (
         <>
+            <h1 className="text-center p-3">Sign Up</h1>
+
+            <Container>
+
+            <Form className="p-4">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter first name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter last name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                </Form.Group>
+                <Image/>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            </Container>
 
         </>
     )
