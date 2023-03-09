a9520d00c1dab60394210715389ad276c3ce6dee
Nicole Diaz <ndiaz18@cnm.edu>
2023-03-09 16:24:38 -0700

    added some fixes on message and navigation

--- frontend/src/ui/Message.jsx
+++ frontend/src/ui/Message.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import {Col, Container, Image, Row} from "react-bootstrap";
+import profilepic from "../images/profile.jpg"
 export function Message () {
     return (
         <>
             <h1 className='text-center'>Messages</h1>
             <div>
-                <Container className='bg-secondary rounded'>
+                <Container className='rounded border-dark border-5 my-2 w-50'>
                     <Row>
-                        <Col xs={9} className='py-2'>
-                            <p>
-                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis</p>
+                        <Col xs={3} className='d-flex justify-content-center'>
+                            <Image fluid src={profilepic} width={100} height={100} alt="meow" className='rounded-circle'/>
                         </Col>
-                            <Col xs={3} className='d-flex justify-content-end px-0'>
-                                <Image fluid src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" width={100} height={100} alt="meow" className='rounded-circle'/>
-                            </Col>
+                        <Col xs={9} className='py-2'>
+                            <h5>Nicole Diaz</h5>
+                            <p>I want this product</p>
+                        </Col>
                     </Row>
                 </Container>
             </div>
