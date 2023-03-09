31f80df3ba07ca3c6db2299ed61a86e6fe6e623c
Felipe Juarez Jr <mr.felipe.juarez.jr@gmail.com>
2023-03-09 16:05:30 -0700

    Added Img links

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -9,22 +9,22 @@ export function Home() {
     return (
         <>
             <h1>Home</h1>
-            {/*<Card>*/}
-            {/*    <Card.Img variant="top" src="holder.js/100px180" />*/}
-            {/*    <Card.Body>*/}
-            {/*        <Card.Text>*/}
-            {/*            Some quick example text to build on the card title and make up the*/}
-            {/*            bulk of the card's content.*/}
-            {/*        </Card.Text>*/}
-            {/*    </Card.Body>*/}
-            {/*</Card>*/}
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
 
                 <Container>
                     <Row>
-                        <Col><Image src={BoardGame} /></Col>
-                        <Col><Image src={Disk} /></Col>
-                        <Col><Image src={Console} /></Col>
-                        <Col><Image src={Cards} /></Col>
+                        <Col><Image src={BoardGame} alt={"Board Game Icon"}/></Col>
+                        <Col><Image src={Disk} alt={"Disk Game Icon"}/></Col>
+                        <Col><Image src={Console} alt={"Console Game Icon"}/></Col>
+                        <Col><Image src={Cards} alt={"Card Game Icon"}/></Col>
                     </Row>
                 </Container>
 
