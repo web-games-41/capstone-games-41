4c8a9723083eb6d7473ab94caa67a986c97e2252
Felipe Juarez Jr <mr.felipe.juarez.jr@gmail.com>
2023-03-09 15:30:25 -0700

    Added GameIcons folder, images and started Home page content

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -5,15 +5,19 @@ export function Home() {
     return (
         <>
             <h1>Home</h1>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            {/*<Card>*/}
+            {/*    <Card.Img variant="top" src="holder.js/100px180" />*/}
+            {/*    <Card.Body>*/}
+            {/*        <Card.Text>*/}
+            {/*            Some quick example text to build on the card title and make up the*/}
+            {/*            bulk of the card's content.*/}
+            {/*        </Card.Text>*/}
+            {/*    </Card.Body>*/}
+            {/*</Card>*/}
+
+
+
+
         </>
     )
 }
