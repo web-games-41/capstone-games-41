457cada6a5e2d57187fb22954788a1a130eea27d
Jared Diaz <jdiaz97@cnm.edu>
2023-03-09 16:06:07 -0700

    working on create-listing

---
+++ frontend/src/ui/CreateListing.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Button, Carousel, Container, Image} from "react-bootstrap";
+import image1 from "../images/createlistingimg1.png"
+import image2 from "../images/createlistingimg2.png"
+import image3 from "../images/createlistingimg3.png"
+import Form from "react-bootstrap/Form";
+export function CreateListing() {
+    return (
+        <>
+            <Container>
+                <Form className="p-4">
+                    <Image className="d-block mx-auto img-thumbnail border border rounded w-50" src={image1}/>
+                    <Form.Group>
+                        <Button className="d-block mx-auto mt-2" variant="outline-secondary">Upload</Button>{' '}
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Name of Item</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Name of Item" />
+                    </Form.Group>
+                    </Form>
+            </Container>
+        </>
+    )
+}
