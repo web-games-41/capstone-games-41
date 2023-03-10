c39b22b5bba9cb0f50a966e7102ac4e0aa32829a
Felipe Juarez Jr <mr.felipe.juarez.jr@gmail.com>
2023-03-09 17:19:00 -0700

    Added Images and text to Home

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -5,13 +5,15 @@ import BoardGame from "./img/gameIcons/boardGames.png"
 import Disk from "./img/gameIcons/disks.png"
 import Console from "./img/gameIcons/consoleCtlr.png"
 import Cards from "./img/gameIcons/cardDeck.png"
+import Add from "./img/gameIcons/addIcon.png"
+import CastleGame from "./img/gameIcons/castleBoard.png"
 
 export function Home() {
     return (
         <>
             <h1 className={'text-center'}>Home</h1>
 
-            <Container className="text-center">
+            <Container className="text-center mb-5">
                 <Row>
                     <Col><Image className="icons" src={BoardGame} alt={"Board Game Icon"}/></Col>
                     <Col><Image className="icons" src={Disk} alt={"Disk Game Icon"}/></Col>
@@ -20,6 +22,15 @@ export function Home() {
                 </Row>
             </Container>
 
+            <div className={"text-center bg-light mb-5"}>
+                    <Image className="mainGameIcon mt-5" src={CastleGame} alt={"Castle Game Icon"}/>
+                {/*LEFT ALIGN THIS TEXT*/}
+                    <p>Name of Item:</p>
+                    <p>Condition:</p>
+                    <p>Category:</p>
+            </div>
+
+            <Image className="addIcon mx-auto d-block pt-5" src={Add} alt="Add Icon Button"/>
         </>
     )
 }
