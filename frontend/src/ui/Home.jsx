5cef1cce1b3b9b3f422d059c364ed44e6818cb37
Felipe Juarez Jr <mr.felipe.juarez.jr@gmail.com>
2023-03-11 11:22:52 -0700

    Deleted commented unused code

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -11,29 +11,6 @@ import listingitem from "../images/createlistingimg1.png"
 
 export function Home() {
     return (
-     /*   <>
-            <h1 className={'text-center'}>Home</h1>
-
-            <Container className="text-center mb-5">
-                <Row>
-                    <Col><Image className="icons" src={BoardGame} alt={"Board Game Icon"}/></Col>
-                    <Col><Image className="icons" src={Disk} alt={"Disk Game Icon"}/></Col>
-                    <Col><Image className="icons" src={Console} alt={"Console Game Icon"}/></Col>
-                    <Col><Image className="icons" src={Cards} alt={"Card Game Icon"}/></Col>
-                </Row>
-            </Container>
-
-            <div className={"text-center bg-light mb-5"}>
-                    <Image className="mainGameIcon mt-5" src={CastleGame} alt={"Castle Game Icon"}/>
-                {/!*LEFT ALIGN THIS TEXT*!/}
-                    <p>Name of Item:</p>
-                    <p>Condition:</p>
-                    <p>Category:</p>
-            </div>
-
-            <Image className="addIcon mx-auto d-block pt-5" src={Add} alt="Add Icon Button"/>
-        </>
-*/
         <>
             <Container className="text-center mt-5">
                 <Row>
--- frontend/src/ui/Profile.jsx
+++ frontend/src/ui/Profile.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {Image} from "react-bootstrap";
+import {Container, Image, Form, Col, Row, Button} from "react-bootstrap";
 import Avatar from "./img/avatar.jpg"
 
 
@@ -7,28 +7,42 @@ import Avatar from "./img/avatar.jpg"
 export function Profile() {
     return (
         <>
+            <Container>
             <h1 className="mb-5">Profile</h1>
                 <div className="ms-5 ps-4 ms-md-0 ps-md-0">
                 <Image fluid className="rounded mb-5 mx-md-auto d-block" src={Avatar} alt="Generic Avatar"/>
                 </div>
-                <form action="" method="" className="text-center">
-                    <div className="mb-4">
-                        <label htmlFor="firstName"></label>
-                        <input className={"me-5"} type="text" name="firstName" id="firstName" required placeholder="First Name" size="12" />
 
+                <Container>
+                    {/*ADD ALL THIS TO A CARD!!!*/}
+                <Form>
+                    <Row>
+                    <Col xs={6}>
+                        <Form.Group controlId={""}>
+                            <Form.Label></Form.Label>
+                            <Form.Control type={"text"} required placeholder={"First Name"}></Form.Control>
+                        </Form.Group>
+                    </Col>
+                    <Col xs={6}>
+                        <Form.Group controlId={""}>
+                            <Form.Label></Form.Label>
+                            <Form.Control type={"text"} required placeholder={"Last Name"}></Form.Control>
+                        </Form.Group>
+                    </Col>
+                        <Form.Group controlId={""}>
+                            <Form.Label></Form.Label>
+                            <Form.Control type={"text"} required placeholder={"Email"}></Form.Control>
+                        </Form.Group>
+                    <Col xs={12} className={"text-center"}>
+                    <Form.Group>
+                        <Button className={"btn btn-light btn-outline-secondary mt-5"}>Update</Button>
+                    </Form.Group>
+                    </Col>
+                    </Row>
+                </Form>
+                </Container>
 
-                        <label htmlFor="Lastname"></label>
-                        <input type="text" name="LastName" id="LastName" required placeholder="Last Name" size="12" />
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="email"></label>
-                        <input type="email" name="email" id="email" required placeholder="Email" size="34" />
-                    </div>
-                    <div className="">
-                        <input type="submit" value="Update" />
-                    </div>
-                </form>
-
+            </Container>
 
         </>
 
