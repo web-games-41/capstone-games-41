1ec543ed43dde2b0b77993a1c8eaff56a6c01471
Felipe Juarez Jr <mr.felipe.juarez.jr@gmail.com>
2023-03-16 13:20:22 -0600

    merging

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,8 @@ import listings from './listing'
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import auth from "./auth"
 import currentUser from "./currentUser.js";
-
-const reducers = {listings:listings, auth:auth, currentUser}
-const reducer = combineReducers(reducers)
-
 import inbox from "./inbox.js";
 
-const reducer = combineReducers({listings:listings, auth:auth, inbox:inbox})
+const reducer = combineReducers({listings:listings, auth:auth, inbox:inbox, currentUser:currentUser})
 
 export const store = configureStore({reducer})
