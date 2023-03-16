e7b702b027c5f4bb358cda1c4d3c7432fe4e39a6
Jared Diaz <jdiaz97@cnm.edu>
2023-03-16 13:11:58 -0600

    created store/slice and formik for create listing

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,8 @@
 import listings from './listing'
 import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import auth from "./auth"
+import categories from "./categories.js";
 
-const reducer = combineReducers({listings:listings, auth:auth})
+const reducer = combineReducers({listings:listings, auth:auth, categories:categories})
 
 export const store = configureStore({reducer})
