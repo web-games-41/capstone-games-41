26051f794f15f1ba052edcf7382d97f360b6c1c0
Felipe Juarez Jr <mr.felipe.juarez.jr@gmail.com>
2023-03-16 20:48:59 -0600

    Added CSS to Game Icons, but commented it all out until further testing

--- frontend/src/ui/home/Home.jsx
+++ frontend/src/ui/home/Home.jsx
@@ -2,11 +2,10 @@ import React from "react"
 import "../App.css"
 import {Card, Col, Container, Row, Image, Button, Form, DropdownButton, Dropdown} from "react-bootstrap";
 import BoardGame from "../img/gameIcons/boardGames.png"
+import BoardGamesHvr from "../img/gameIcons/boardGamesHvr.png"
 import Disk from "../img/gameIcons/disks.png"
 import Console from "../img/gameIcons/cnslCtlr.png"
 import Cards from "../img/gameIcons/cardDeck.png"
-import Add from "../img/gameIcons/addIcon.png"
-import listingItem from "../../images/createlistingimg1.png"
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAllListings} from "../../store/listing.js";
 import {HomeCard} from "./HomeCard.jsx";
@@ -31,10 +30,13 @@ export function Home() {
         <>
             <Container className="text-center mt-5">
                 <Row>
-                    <Col><Button className={"btn btn-light btn-outline-info"}><Image className="icons" src={BoardGame} alt={"Board Game Icon"}/></Button></Col>
-                    <Col><Button className={"btn btn-light btn-outline-info"}><Image className="icons" src={Disk} alt={"Disk Game Icon"}/></Button></Col>
-                    <Col><Button className={"btn btn-light btn-outline-info"}><Image className="icons" src={Console} alt={"Console Game Icon"}/></Button></Col>
-                    <Col><Button className={"btn btn-light btn-outline-info"}><Image className="icons" src={Cards} alt={"Card Game Icon"}/></Button></Col>
+                    <Col className="img"><Button className={"bg-transparent border-0"}>
+                        <Image className="icons" src={BoardGame} alt={"Board Game Icon"}/>
+                        {/*<Image className="hoverImg" src={BoardGamesHvr} alt={"Board Game Icon"}/>*/}
+                    </Button></Col>
+                    <Col><Button className={"bg-transparent border-0"}><Image className="icons" src={Disk} alt={"Disk Game Icon"}/></Button></Col>
+                    <Col><Button className={"bg-transparent border-0"}><Image className="icons" src={Console} alt={"Console Game Icon"}/></Button></Col>
+                    <Col><Button className={"bg-transparent border-0"}><Image className="icons" src={Cards} alt={"Card Game Icon"}/></Button></Col>
                 </Row>
             </Container>
 
